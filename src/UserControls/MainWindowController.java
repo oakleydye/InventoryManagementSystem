21e063ded3257d5c8657c671fe52f9821d44e877
Oakley Dye <oakley.dye@gmail.com>
2020-09-01 22:19:53 -0600

    apparently controllers cant have constructors

--- src/UserControls/MainWindowController.java
+++ src/UserControls/MainWindowController.java
@@ -21,11 +21,11 @@ public class MainWindowController {
     private final ObservableList<Product> products = FXCollections.observableArrayList();
     public Inventory inventory;
 
-    public MainWindowController(){
+    /*public MainWindowController(){
         this.inventory = new Inventory();
         this.parts.setAll(this.inventory.getAllParts());
         this.products.setAll(this.inventory.getAllProducts());
-    }
+    }*/
 
     public void btnDeletePart_Click(ActionEvent actionEvent) {
         Part selectedPart = grdParts.getSelectionModel().getSelectedItem();
